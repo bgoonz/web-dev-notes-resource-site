5ea534894c296c673efcdb1bf63b60dea23b2a71
bryan <bryan.guner@gmail.com>
2021-01-18 02:32:55 -0500

    push

---
+++ 1-tools/node-npm/npm.broofa.com/js/index.js
@@ -0,0 +1,20 @@
+import { Flash } from './Components.js';
+import App from './App.js';
+import { html, render } from '/vendor/preact.js';
+
+// Used to feature-detect that es6 modules are loading
+window.indexLoaded = true;
+
+window.addEventListener('error', err => {
+  console.error(err);
+  Flash(err.message);
+});
+
+window.addEventListener('unhandledrejection', err => {
+  console.error(err);
+  Flash(err.reason);
+});
+
+window.onload = function() {
+  render(html`<${App} />`, document.body);
+};
